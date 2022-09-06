2d4b68d4bb1893f6b13e44fae1d37f2f294761a3
Henry Gillinger <henry.gillinger@ga.ntig.se>
2022-09-06 15:58:01 +0200

    2commit

--- Interactive_story/Program.cs
+++ Interactive_story/Program.cs
@@ -5,50 +5,76 @@ string name = Console.ReadLine();
 
 
 Console.WriteLine($"trevligt att träffas {name} vi behöver din hjälp att rädda byns bagare! vill du hjälpa oss? ");
-
+Console.WriteLine("a= ja, b= nej");
 string bra = Console.ReadLine();
 
-if (bra == "ja")
+if (bra == "a")
 
 {
 
     Console.WriteLine("wow vad modigt, bagaren har blivit kidnappad av den onda kungen. Det finns två vägar till kungens slott.");
+    Console.WriteLine("vilken väg vill du ta? vill du gå genom den mörka skogen eller segla över sjön? ");
+    Console.WriteLine(" a= skogen, b= sjön");
 
-    Console.WriteLine("vilken väg vill du ta? vill du gå genom den mörka skogen eller segla över sjön?");
+    string väg = Console.ReadLine();
 
-     string väg = Console.ReadLine();
-
-    if (väg == "skogen")
+    if (väg == "a")
 
     {
 
         Console.WriteLine("du fortsätter genom den mörka skogen. något lyser från marken, Det är ett svärd! du tar med det ifall.");
         Console.WriteLine("du kommer fram till slotts dörren men någon står och väntar framför den.");
+        Console.WriteLine("vill du använda ditt svärd för att dräpa vakten eller vill försöka övertala honnom att låta dig gå in i slottet?");
+        Console.WriteLine("a= använd svärdet, b= övertala honnom");
+        string svärd = Console.ReadLine();
+        if (svärd == "a")
+        {
+            Console.WriteLine("vakten ber om sitt liv, Han har en familj! men du slår ner honnom med ditt svärd");
+            Console.WriteLine("du fortsätter in i slottet. Där ser du bagaren, men han värkar göra kakor till invånarna av byn med gift i dem! ");
+            Console.WriteLine("vill du döda bakaren eller prata med honnom");
+            Console.WriteLine("a=döda, b= prata");
+            string potatis = Console.ReadLine();
+            if (potatis == "a")
+            {
+                Console.WriteLine($" {namn} lyfter sitt svärd och tar bagarens liv. byn är räddad men vad kostade det?");
 
+            }
+
+            else if (potatis == "b")
+
+            {
+                Console.WriteLine($"du försöker prata med bagaren men du är fylld av bloodlust,")
+
+
+
+        }
+
+
+        }
 
 
     }
 
-    else if ( väg == "sjön")
-
-    { 
-
-        Console.WriteLine("du går ner mot stranden och dar upp den rostiga båten. ");
-        Console.WriteLine( "du får klump i magen när du kollar ut över de svarta vattnet");
-
-    }
-
-    else 
+    else if (väg == "b")
 
     {
 
-     Console.WriteLine( "nu blev det fel, det kanske var ett stavfel eller nåt men du måste börja om nu :(");
+        Console.WriteLine("du går ner mot stranden och dar upp den rostiga båten. ");
+        Console.WriteLine("du får klump i magen när du kollar ut över de svarta vattnet");
+
+    }
+
+    else
+
+    {
+
+        Console.WriteLine("nu blev det fel, du måste börja om nu :(");
 
     }
 
 }
 
-else if (bra == "nej")
+else if (bra == "b")
 
 {
 
